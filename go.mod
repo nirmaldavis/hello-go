765901e4937b2239197df20c3e9f0e182542c108
Nirmal Davis V <Nirmal.Davis@med.ge.com>
2022-10-08 23:33:05 +0530

    Used a custom sample Go module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nirmaldavis/hello-go
 
 go 1.17
+
+require github.com/nirmaldavis/mylogger-go v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/nirmaldavis/mylogger-go"
+)
 
 func main() {
 	fmt.Println("Hello Go..!!")
+
+	mylogger.LogInfo("I am using a Go module")
+	mylogger.LogWarning("Working .. ?!!")
+	mylogger.LogError("Filed to work :-( ")
 }
